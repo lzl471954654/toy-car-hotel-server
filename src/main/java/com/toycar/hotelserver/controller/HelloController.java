500c0fdff178ee421569061aa278d64f00d7a3e4
YL <2582167435@qq.com>
2019-03-18 21:13:16 +0800

    将Department中select方法注释掉 程序可以正常运行

--- src/main/java/com/toycar/hotelserver/controller/HelloController.java
+++ src/main/java/com/toycar/hotelserver/controller/HelloController.java
@@ -1,14 +1,11 @@
 package com.toycar.hotelserver.controller;
 
-import com.toycar.hotelserver.mapper.StaffMapper;
 import com.toycar.hotelserver.mapper.StockMapper;
-import com.toycar.hotelserver.pojo.Staff;
 import com.toycar.hotelserver.pojo.Stock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
